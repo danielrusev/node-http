f837cc36e667f923b610e251e6b8e37e0359f155
Daniel Rusev <daniel.rusev@acenture.com>
2018-09-27 10:38:44 +0100

    exception logging

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 var http = require('http');
 var fileSystem = require('fs');
 
+const Sentry = require('@sentry/node');
+Sentry.init({ dsn: 'https://92e081050080418a8a6124df5e10435e@sentry.io/1289615' });
+
 var server = http.createServer(function(req, resp){
 	fileSystem.readFile('./index.html', function(error, fileContent){
 		if(error){
