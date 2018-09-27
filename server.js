87c36efb611c4e744ffe7cd9dff6385b76c3c1b7
Daniel Rusev <daniel.rusev@acenture.com>
2018-09-27 11:01:07 +0100

    release tracking

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ var fileSystem = require('fs');
 var Raven = require('raven');
 Raven.config('http://cfbb97d606394c82a7fd4b040970db5a@35.228.87.41:9000/3').install();
 
+
+// See SDK documentation for language specific usage.
+Raven.config('http://cfbb97d606394c82a7fd4b040970db5a@35.228.87.41:9000/3', {
+	release: '0e4fdef81448dcfa0e16ecc4433ff3997aa53572'
+  }).install();
+  
+
 var server = http.createServer(function(req, resp){
 	fileSystem.readFile('./index.html', function(error, fileContent){
 		if(error){
